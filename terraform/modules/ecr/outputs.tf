f75e0c06744815df705e61cf6044648ff65597ee
Reddy Prasad Duggineni <reddyprasadduggineni@gmail.com>
2026-05-23 18:21:12 -0400

    feat: add ECR module

---
+++ terraform/modules/ecr/outputs.tf
@@ -0,0 +1,38 @@
+# terraform/environments/dev/outputs.tf
+
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster API endpoint"
+  value       = module.eks.cluster_endpoint
+  sensitive   = true
+}
+
+output "cluster_certificate_authority" {
+  description = "EKS cluster certificate authority data"
+  value       = module.eks.cluster_certificate_authority
+  sensitive   = true
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnet_ids
+}
+
+output "ecr_repository_urls" {
+  description = "ECR repository URLs"
+  value       = module.ecr.repository_urls
+}
+
+output "configure_kubectl" {
+  description = "Command to configure kubectl"
+  value       = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
+}
